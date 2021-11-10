fff03a9a7463db7b5123188f23195b9d608c9ccf
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-11-10 18:30:01 +0300

    Добавлен проект мигратор

---
+++ RandomFilmServer.Migrator/Migrations/TemplateMigration.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+
+namespace RandomFilmServer.Migrator.Migrations
+{
+    [Migration(202010010720)]
+    public class TemplateMigration : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
